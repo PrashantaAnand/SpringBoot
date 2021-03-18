52b8c957a99da107190be7b6c28112fce71d510a
PrashantAnand1 <prasant.anand@nectarinfotel.com>
2021-03-18 11:08:58 +0530

    hellon ew

--- src/main/java/com/practice/controller/MyController.java
+++ src/main/java/com/practice/controller/MyController.java
@@ -19,7 +19,7 @@ public class MyController {
 		System.out.println("inside about handler");
 		return "about";
 	}
-	///hello
+	///hellogg
 	@GetMapping("/looping")
 	public String looping(Model model)
 	{
